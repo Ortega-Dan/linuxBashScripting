8f827f3c5e41427d71dcf9cc36628eabf291cabc
Dan Ortega <ortega.dan2010@gmail.com>
2020-02-08 10:59:11 -0500

    organizing

---
+++ bash/forLoopSimple.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# One liner will be
+# for i in {1..50}; do echo $i; done
+
+# the following script prints all numbers between 1 and 50
+
+# the range is between the two numbers
+for i in {1..50}
+do
+    echo $i
+done
