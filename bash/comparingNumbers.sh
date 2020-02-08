5bde51d4c3d21df5f4939b1e4fb40cdcfcc1bc35
Dan Ortega <ortega.dan2010@gmail.com>
2020-02-08 11:19:21 -0500

    Number comparisons with nested if and else

--- bash/comparingNumbers.sh
+++ bash/comparingNumbers.sh
@@ -1,7 +1,16 @@
+
+# Number comparisons with nested if and else
+
 read x
 read y
 
 if ((x > y))
 then
     echo "X is greater than Y"
+else
+    if ((x == y))
+    then echo "X is equal to Y"
+    else
+        echo "X is less than Y"
+    fi
 fi
